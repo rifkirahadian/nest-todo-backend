9e53e79e7c79ee5432dd24555b7e1917e626e669
Rifki Rahadian <rrahadian94@gmail.com>
2024-07-04 05:41:12 +0700

    chore: install swagger

--- src/database/migrations/20240703141449-create-notifications-table.js
+++ src/database/migrations/20240703141449-create-notifications-table.js
@@ -27,6 +27,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       updatedAt: Sequelize.DATE,
       createdAt: Sequelize.DATE,
     });
