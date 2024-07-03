db57e7705c17acbb4a705f73956ed0d799848279
Rifki Rahadian <rrahadian94@gmail.com>
2024-07-04 06:51:01 +0700

    fix: remove unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
