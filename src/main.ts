4dd95bc3865d45737c0c81a809c89a6862d27287
Rifki Rahadian <rrahadian94@gmail.com>
2024-07-06 12:20:39 +0700

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
